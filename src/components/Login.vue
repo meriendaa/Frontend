75da482367e5d511991c4c1a0274fece9c3df721
Eloi Alonso Quiñonero <97552454+Develoi89@users.noreply.github.com>
2024-08-19 10:38:11 +0200

    tittle and icon modified

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,23 +1,32 @@
 <template>
 	<div class="container d-flex justify-content-center align-items-center vh-100">
-    <div class="card p-4 shadow-sm" style="max-width: 400px; width: 100%;">
+        <div class="card p-4 shadow-sm" style="max-width: 400px; width: 100%;">
         <h3 class="text-center mb-4">Login</h3>
-        <form>
             <div class="mb-3">
                 <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="email" placeholder="Enter your email">
+                <input v-model="mail" type="email" class="form-control" id="email" placeholder="Enter your email">
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" placeholder="Enter your password">
+                <input v-model="psw" type="password" class="form-control" id="password" placeholder="Enter your password">
             </div>
-            <button type="submit" class="btn btn-primary w-100">Login</button>
+            <button @click="test" type="submit" class="btn btn-primary w-100">Login</button>
             <div class="mt-3 text-center">
-                <a href="#">Forgot password?</a>
+                <router-link to="/about">Forgot your password?</router-link>
             </div>
-        </form>
 		
+        </div>
     </div>
-</div>
 
 </template>
+<script setup>
+    import { ref } from 'vue';
+    const mail = ref();
+    const psw = ref();
+    function test()
+    {
+        console.log(mail.value);
+        console.log(psw.value);
+    }
+    
+</script>
